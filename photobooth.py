406585d360540666dd9071fb69ce81af7f6ebb44
Yash Puneet <yash.puneet02@gmail.com>
2026-09-25 21:05:26 -0700

    Add asset overlay to live video

--- photobooth.py
+++ photobooth.py
@@ -1,4 +1,32 @@
 import cv2
+import numpy as np
+import os
+
+# Constants
+GHOST = "assets/ghost.webp"
+
+if not os.path.exists(GHOST):
+    print(f"Error: '{GHOST}' not found")
+    exit()
+
+# Overlay logic
+def overlay(background, overlay):
+    bg = background.copy()
+
+    # Normalize Alpha
+    if overlay.shape[2] == 4:
+        b, g, r, alpha = cv2.split(overlay)
+        alpha_mask = alpha/255.0
+        inv_mask = 1.0 - alpha_mask
+
+        # Blend
+        for c in range(0, 3):
+            bg[:,:,c] = (alpha_mask * overlay[:,:,c] + inv_mask * bg[:,:,c])
+        
+        return bg
+
+    else:
+        return background
 
 # Initialize the default webcam
 cap = cv2.VideoCapture(0)
@@ -18,8 +46,15 @@ while True:
         print("Error: Failed to grab frame.")
         break
 
+    h, w, _ = frame.shape
+
+    ghost = cv2.imread(GHOST, cv2.IMREAD_UNCHANGED)
+    ghost_resized = cv2.resize(ghost, (w,h), interpolation=cv2.INTER_AREA)
+
+    altered_frame = overlay(frame, ghost_resized)
+
     # Display
-    cv2.imshow('Horror Photobooth', frame)
+    cv2.imshow('Horror Photobooth', altered_frame)
 
     # if 'q' is pressed, exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
